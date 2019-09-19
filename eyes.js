8b9166ff949e3e7bf221a3b5d7dab9aa0b75f1f0
John Mwaura <johnmwaura08@gmail.com>
2019-09-18 19:56:59 -0500

    eye mvmt to the left, make sure you read the comments man

---
+++ eyes.js
@@ -0,0 +1,12 @@
+const balls = document.getElementsByClassName("ball");
+
+document.onmousemove = function(){
+    var x = event.clientX * 100 / window.innerWidth + "%"; //gets horizontal coordinate of onmouse move and the window.innerwidth gets the browser width
+    var y = event.clientY * 100 / window.innerHeight + "%";//gets vertical coordinate of onmouse move and the window.innerheight gets the browser height
+
+
+for( var i =0; i< 2; i++){
+    balls[i].style.left =x;
+}
+
+}
